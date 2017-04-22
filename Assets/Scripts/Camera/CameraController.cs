5d11e49ed4b4ce9854f5e9f73c7c9688be7b3024
Zak <zwrawr@gmail.com>
2017-04-22 03:46:53 +0100

    World zoom

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float zoom_speed = 0.01f;
+    private float init_zoom;
+
+    private Camera cam;
+	// Use this for initialization
+	void Start () {
+        this.cam = this.GetComponent<Camera>();
+
+        this.init_zoom = cam.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.cam.orthographicSize += zoom_speed * Time.deltaTime;
+
+	}
+}
